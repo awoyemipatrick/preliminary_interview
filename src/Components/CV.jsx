360d2492270f1090983feacee3cc28fcabe474f3
Awoyemi Patrick <awoyemipatrick33@yahoo.com>
2023-05-11 13:45:07 +0100

    Modify & Styled CV

--- src/Components/CV.jsx
+++ src/Components/CV.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { MdEmail, MdPhone } from 'react-icons/md'
 
 
 const CV = () => {
 
   return (
-    <main className='flex flex-col items-center'>
+    <main className='flex flex-col items-center '>
       <h1 className='text-[24px] font-bold my-8 '>My CV </h1>
-      <div className='flex flex-col items-center mx-[20%] text-black bg-white'>
-        <div className='flex flex-col items-center'>
-          <h1 className='text-[34px] font-bold'>Awoyemi Patrick Gbenga</h1>
-          <p>Front-End Engineer</p>
+      <div className='flex flex-col items-center mx-[20%] text-black bg-white my-[2rem] '>
+        <div className='flex flex-col items-center '>
+          <h1 className='lg:text-[40px] text-[1.5rem] font-bold mt-[1rem] '>Awoyemi Patrick Gbenga</h1>
+          <p className='text-[1.3rem] opacity-50 mt-[1rem] '> Front-End Engineer</p>
         </div>
         <div>
-          <div className='flex justify-center '>
-            <a className='mr-4' href="">0-816-118-5089</a>
-            <a className='ml-4' href="">awoyemipatrick33@yahoo.com</a>
+          <div className=' justify-center text-center lg:flex px-[1rem] '>
+            <a className='my-8 mx-4 p-2 bg-[#03989e] text-white rounded-2xl flex justify-center items-center ' href=""> <span className='text-white mx-2 bg-black p-[0.3em] rounded-2xl text-[1.2rem] ' ><MdPhone /> </span> 0-816-118-5089</a>
+            <a className='my-8 mx-4 p-2 bg-[#03989e] text-white rounded-2xl flex justify-center items-center ' href=""> <span className='text-black mx-2 text-[2rem] ' ><MdEmail /></span> awoyemipatrick33@yahoo.com</a>
           </div>
 
           <div>
-            <p>
+            <p className='mx-[1rem] text-[1rem] tracking-wide '>
               I have an understanding of web development technologies such as HTML, CSS, and JavaScript. I am
               proficient in using JavaScript frameworks such as ReactJS. I have experience building responsive
               and mobile-friendly web pages and am able to effectively use CSS preprocessors such as SASS. I am
@@ -29,16 +30,16 @@ const CV = () => {
             </p>
           </div>
 
-          <div className='items-center flex flex-col mx-[2.5rem]  my-6'>
-            <h2 className='font-bold text-[18px] my-4 '>EXPERIENCE</h2>
-            <div className='flex'>
-              <div className=''>
-                <h4>Front-End Mobile App Developer</h4>
-                <p>
+          <div className='items-center flex flex-col  my-6'>
+            <h2 className='font-bold text-[18px] my-8 underline decoration-[#03989e] decoration-4 '>EXPERIENCE</h2>
+            <div className='lg:flex justify-between '>
+              <div className='mx-[2rem] lg:max-w-[40%] mb-8 '>
+                <h4 className='font-bold'>Front-End Mobile App Developer</h4>
+                <p className='leading-8 opacity-50'>
                   BINCOM ICT SOLUTIONS <br />
                   May 2021 -Present
                 </p>
-                <h4>Role / Focus</h4>
+                <h4 className='font-bold my-2'>Role / Focus</h4>
                 <ul>
                   <li className='list-disc'>
                     Building and managing interactive websites
@@ -69,13 +70,13 @@ const CV = () => {
                 </ul>
               </div>
 
-              <div>
-                <h4>Technical Assistant (Intern)</h4>
-                <p>
-                  IJTEK POWER SOLUTIONS
+              <div className='mx-[2rem] lg:max-w-[40%]'>
+                <h4 className='font-bold'>Technical Assistant (Intern)</h4>
+                <p className='leading-8 opacity-50'>
+                  IJTEK POWER SOLUTIONS <br />
                   March 2023 -Present
                 </p>
-                <h4>Role / Focus</h4>
+                <h4 className='font-bold my-2'>Role / Focus</h4>
                 <ul>
                   <li className='list-disc'>
                     Building and managing interactive websites.
@@ -101,66 +102,70 @@ const CV = () => {
             </div>
           </div>
 
-          <div className='flex my-4'>
-            <div className='flex flex-col w-[50%] '>
-              <h2 className='font-bold text-[18px] '>TRAINING & DEVELOPMENT</h2>
-              <h4>
-                Bincom Global Tech
-              </h4>
-              <h4> May 2021 - Present</h4>
-              <p>
-                BIncom Dev Center Is a Multi-location based
-                Training and Development Center for
-                Technology Resources and Technology
-                Solutions.
-              </p>
-            </div>
+          <div className=' flex-col lg:mx-[2rem] mx-4 mt-[4rem]'>
 
-            <div className='flex flex-col w-[50%] '>
-              <div className='flex flex-col items-center'>
-                <h4 className='font-bold text-[18px] '>EDUCATION</h4>
-                <h4>Bincom Academy(Front-End Mobile App Class)</h4>
-                <p>May 2021 -Present</p>
+            <div className='lg:flex mx-4 '>
+
+              <div className=' lg:w-[50%] mb-[2rem] '>
+                <h2 className='font-bold text-[18px] underline decoration-[#03989e] decoration-4 '>TRAINING & DEVELOPMENT</h2>
+                <h4>
+                  Bincom Global Tech
+                </h4>
+                <h4> May 2021 - Present</h4>
+                <p>
+                  BIncom Dev Center Is a Multi-location based
+                  Training and Development Center for
+                  Technology Resources and Technology
+                  Solutions.
+                </p>
               </div>
-              <div className='flex flex-col mx-[4rem] '>
-                <div className='m-2 p-2 rounded-2xl bg-[#03989e] text-white self-start'>
-                  <p className=''>
-                    Mobile Beginners Class
+
+              <div className='flex flex-col lg:w-[50%]'>
+                <div className='flex flex-col items-center'>
+                  <h4 className='font-bold text-[18px] underline decoration-[#03989e] decoration-4'>EDUCATION</h4>
+                  <h4>Bincom Academy(Front-End Mobile App Class)</h4>
+                  <p>May 2021 -Present</p>
+                </div>
+                <div className='lg:flex flex-col lg:mx-[1.5rem] mx-[8%]  '>
+                  <div className='m-2 p-2 rounded-2xl bg-[#03989e] text-[12px] text-white flex justify-center items-center self-start'>
+                    <p className=''>
+                      Mobile Beginners Class
+                    </p>
+                  </div>
+                  <p className='m-2 p-2 rounded-2xl bg-[#03989e] text-[12px] text-white flex justify-center items-center self-end'>
+                    Mobile Intermediate Class
+                  </p>
+                  <p className='m-2 p-2 rounded-2xl bg-[#03989e] text-[12px] text-white flex justify-center items-center self-start'>
+                    Advanced Front-End Class
                   </p>
                 </div>
-                <p className='m-2 p-2 rounded-2xl bg-[#03989e] text-white self-end'>
-                  Mobile Intermediate Class
-                </p>
-                <p className='m-2 p-2 rounded-2xl bg-[#03989e] text-white self-start'>
-                  Advanced Front-End Class
-                </p>
-              </div>
-              <div className='flex flex-col items-center'>
-                <h3 className='font-bold '>
-                  Secondary School certificate
-                </h3>
-                <h4 className='text-[12px] '>
-                  Deyo-Tuwo Comprehensive High School, Ododeyo-Imewuro
-                </h4>
-                <p>2002</p>
+                <div className='flex flex-col items-center my-4'>
+                  <h3 className='font-bold '>
+                    Secondary School certificate
+                  </h3>
+                  <h4 className='text-[12px] '>
+                    Deyo-Tuwo Comprehensive High School, Ododeyo-Imewuro
+                  </h4>
+                  <p>2002</p>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className='items-center flex flex-col mx-[8%] '>
-            <h2 className='font-bold text-[18px] '>SOFT/TECHNICAL SKILLS</h2>
-            <div className='flex justify-between flex-wrap '>
-              <p className='m-2 p-2  rounded-2xl bg-[#03989e] text-white ' > Version Control Systems: Git, GitLab, GitHub </p>
-              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl '> HTML</p>
-              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl '>Javscript</p>
-              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl '>CSS</p>
-              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl '>Responsive Web Design</p>
-              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl '>Technical Documentation</p>
-              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl '>Team Collaboration</p>
-              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl '>Content Creation Tools</p>
-              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl '>Analytical & Problem solving</p>
-              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl '>Critical-Thinking</p>
-              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl '>ReactJS</p>
+          <div className='items-center flex flex-col mx-[8%] my-6 '>
+            <h2 className='font-bold text-[18px] underline decoration-[#03989e] decoration-4'>SOFT/TECHNICAL SKILLS</h2>
+            <div className='lg:flex justify-around flex-wrap  '>
+              <p className='m-2 p-2  rounded-2xl bg-[#03989e] text-white text-[12px] flex justify-center items-center ' > Version Control Systems: Git, GitLab, GitHub </p>
+              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl text-[12px] flex justify-center items-center '> HTML</p>
+              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl text-[12px] flex justify-center items-center '>Javscript</p>
+              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl text-[12px] flex justify-center items-center '>CSS</p>
+              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl text-[12px] flex justify-center items-center '>Responsive Web Design</p>
+              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl text-[12px] flex justify-center items-center '>Technical Documentation</p>
+              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl text-[12px] flex justify-center items-center '>Team Collaboration</p>
+              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl text-[12px] flex justify-center items-center '>Content Creation Tools</p>
+              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl text-[12px] flex justify-center items-center '>Analytical & Problem solving</p>
+              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl text-[12px] flex justify-center items-center '>Critical-Thinking</p>
+              <p className='m-2 p-2 bg-[#03989e] text-white rounded-2xl text-[12px] flex justify-center items-center '>ReactJS</p>
             </div>
           </div>
 
