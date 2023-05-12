2bdc2ef0b940dbad115aa5a1099595284fcf6a16
Awoyemi Patrick <awoyemipatrick33@yahoo.com>
2023-05-12 09:09:09 +0100

    tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  purge: {
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    options: {
+      whitelist: [''],
+    },
+  },
   theme: {
     extend: {
       backgroundImage: {
@@ -15,4 +22,5 @@ module.exports = {
     },
   },
   plugins: [],
-}
+  // ...other Tailwind configuration
+};
